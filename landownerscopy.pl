258e61e8fcfe08fd32f368ed3ad57cf2caf850fb
huynhnhattoan <toanhuynhnhat@gmail.com>
2020-07-03 14:51:19 +0700

    add index.html| modified landowners.pl

--- landownerscopy.pl
+++ landownerscopy.pl
@@ -1,11 +1,1 @@
- Volume in drive C has no label.
- Volume Serial Number is A28C-B86A
-
- Directory of C:\Users\toanh\OneDrive\M y t¡nh\MyProject
-
-07/03/2020  02:31 PM    <DIR>          .
-07/03/2020  02:31 PM    <DIR>          ..
-07/03/2020  02:30 PM               132 landowners
-07/03/2020  02:31 PM                 0 landownerscopy.pl
-               2 File(s)            132 bytes
-               2 Dir(s)  183,954,964,480 bytes free
+change1
